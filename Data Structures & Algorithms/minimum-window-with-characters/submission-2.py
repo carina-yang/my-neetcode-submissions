dacc3ec37bf526f5939eb1be66440c1e0088f832
Carina Yang <70391675+carina-yang@users.noreply.github.com>
2026-05-01 11:20:30 -0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = {}
+        for c in t:
+            need[c] = need.get(c, 0) + 1
+
+        window = {}
+
+        required = len(need)
+        formed = 0
+
+        left = 0
+        best_len = float("inf")
+        best_start = 0
+
+        for right in range(len(s)):
+            char = s[right]
+
+            window[char] = window.get(char, 0) + 1
+
+            if char in need and window[char] == need[char]:
+                formed += 1
+
+            while formed == required:
+                if right - left + 1 < best_len:
+                    best_len = right - left + 1
+                    best_start = left
+
+                left_char = s[left]
+                window[left_char] -= 1
+
+                if left_char in need and window[left_char] < need[left_char]:
+                    formed -= 1
+
+                left += 1
+
+        return "" if best_len == float("inf") else s[best_start:best_start + best_len]
+
