cc7fa52373da596e31e0d2a75a727f7f671bd576
Carina Yang <70391675+carina-yang@users.noreply.github.com>
2026-05-18 15:38:36 -0400

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.py
@@ -0,0 +1,37 @@
+import heapq
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        count = {}
+
+        for card in hand:
+            if card not in count:
+                count[card] = 0
+            count[card] += 1
+        
+        unique_hand = list(set(hand))
+        heapq.heapify(unique_hand)
+
+        cur_group = []
+
+        while unique_hand:
+            val = heapq.heappop(unique_hand)
+
+            if cur_group and val != cur_group[-1] + 1:
+                return False
+
+            count[val] -= 1
+            cur_group.append(val)
+
+            if len(cur_group) == groupSize:
+                print(cur_group)
+                for i in cur_group:
+                    if count[i] > 0:
+                        heapq.heappush(unique_hand, i)
+                cur_group = []
+            
+        if cur_group:
+            return False
+            
+        return True
+            
